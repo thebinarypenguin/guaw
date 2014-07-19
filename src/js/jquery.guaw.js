thebinarypenguin/guaw

commit d898ad434469fa8480d908da12d750ce4fffee60

fetchActivity now resolves correctly when there's no data

src/js/jquery.guaw.js
@@ -745,15 +745,14 @@
               this.widgetBody.append(content);
             }
 
-            // Parse the "next" url out of the Link header
+            // If Link header contains a "next" url then fetch it
             matches = /<(\S+)>; rel="next"/.exec(xhr.getResponseHeader('Link'));
-
             if (matches) {
               fetch(matches[1]);
-            } else {
-              master.resolve();
             }
           }
+
+          master.resolve();
         });
 
         // Failure, ...

commit a38b72c628f978548d043fd3321912b1b513f874

Fix fetchActivity() promise resolving too soon
- The promise returned by fetchActivity() was being set as resolved after
the first ajax request
- Ironically this bug was introduced in d898ad434469fa8480d908da12d750ce4fffee60

src/js/jquery.guaw.js
@@ -747,14 +747,17 @@
               this.widgetBody.append(content);
             }
 
-            // If Link header contains a "next" url then fetch it
+            // If Link header contains a "next" url then fetch it, otherwise we're done
             matches = /<(\S+)>; rel="next"/.exec(xhr.getResponseHeader('Link'));
             if (matches) {
               fetch(matches[1]);
+            } else {
+              master.resolve();
             }
+          } else {
+            // No data, probably got "304 Not Modified"
+            master.resolve();
           }
-
-          master.resolve();
         });
 
         // Failure, ...